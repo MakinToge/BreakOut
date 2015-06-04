176c9068da98392cb7bf68875fb8a9364bdc8920
Adrien Veroone <adrien.veroone@isen-lille.fr>
2015-06-04 10:57:54 +0200

    initial work import

--- BreakOut/BreakOut/BreakOut/Program.cs
+++ BreakOut/BreakOut/BreakOut/Program.cs
@@ -1,17 +1,13 @@
 using System;
 
-namespace BreakOut
-{
+namespace BreakOut {
 #if WINDOWS || XBOX
-    static class Program
-    {
+    static class Program {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main(string[] args)
-        {
-            using (Game1 game = new Game1())
-            {
+        static void Main(string[] args) {
+            using (BreakOut game = new BreakOut()) {
                 game.Run();
             }
         }
--- BreakOut/BreakOut/BreakOut/Properties/AssemblyInfo.cs
+++ BreakOut/BreakOut/BreakOut/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 // On Windows, the following GUID is for the ID of the typelib if this
 // project is exposed to COM. On other platforms, it unique identifies the
 // title storage container when deploying this assembly to the device.
-[assembly: Guid("6f53c59d-a0ae-47e7-bfcf-4c47ec753db6")]
+[assembly: Guid("1ba86ef0-b167-4c31-83eb-e69c49e5585e")]
 
 // Version information for an assembly consists of the following four values:
 //
